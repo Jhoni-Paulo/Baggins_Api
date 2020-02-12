8c86fa7c314b3d5676d2e6e8ba8a3cef9edbf300
JhoniDeveloper <jhoni.up.2015@gmail.com>
2020-02-11 23:00:25 -0300

    - Modificações de estrutura  - Adicionando config do banco

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+require('dotenv/config')
+
+module.exports = {
+  dialect: 'mysql',
+  host: process.env.MYSQL_HOST ? process.env.MYSQL_HOST : '127.0.0.1',
+  database: process.env.MYSQL_DATABASE ? process.env.MYSQL_DATABASE : 'bagginsdb',
+  username: process.env.MYSQL_USERNAME ? process.env.MYSQL_USERNAME : 'root',
+  password: process.env.MYSQL_PASSWORD ? process.env.MYSQL_PASSWORD : 'admin'
+};
